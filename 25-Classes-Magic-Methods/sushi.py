24c89808e01733082ad88bcddcd34bae0d55442e
Qossim Afolayan <qossimafolayan@gmail.com>
2020-08-19 21:44:30 +0100

    Doctring

---
+++ 25-Classes-Magic-Methods/sushi.py
@@ -0,0 +1,27 @@
+#Doctsrting is a regular python string that creates technical documentation for 
+#a piece of your program.
+"""
+A module related to the joy of Sushi
+No fishy code found here
+"""
+
+def fish():
+    """
+    Determines if fish is a goos meal choice 
+    Always returns True, because it always is.
+    """
+    return True
+
+class Salmon():
+    """
+    Blueprint for a Salmon object
+    """
+    def __init__(self):
+        self,tastiness = 10
+
+    def bake(self):
+        """
+        Bake the fish in an oven
+        """
+        self.tastiness += 1
+
