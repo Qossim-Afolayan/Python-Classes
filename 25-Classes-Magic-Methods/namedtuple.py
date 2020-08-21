645a192f2bd37bc229c6184efb4543f8346f5089
Qossim Afolayan <qossimafolayan@gmail.com>
2020-08-21 11:51:10 +0100

    Exercxise on namedtuple

--- 25-Classes-Magic-Methods/namedtuple.py
+++ 25-Classes-Magic-Methods/namedtuple.py
@@ -10,3 +10,27 @@ print(animal_farm[0])
 print(gatsby[1])
 print(animal_farm.title)
 print(gatsby.author)
+
+# Use the namedtuple function to create a GymExercise class whose instances
+# will have three attributes: name, weight, and reps.
+
+# Assign a squat variable to a GymExercise tuple object with 
+# a name of “squat”, a weight of 265, and a rep count of 3.
+
+# Assign a bench variable to a GymExercise tuple object with 
+# a name of “benchpress”, a weight of 185, and a rep count of 5.
+
+# Assign a deadlift variable to a GymExercise tuple object with
+# a name of “deadlift”, a weight of 225, and a rep count of 6.
+
+# Assign a press variable to a GymExercise tuple object with 
+# a name of “press”, a weight of 120, and a rep count of 8.
+
+GymExercise = collections.namedtuple("GymExercise", "name weight reps")
+
+squat = GymExercise("squat", 265, 3)
+bench = GymExercise("benchpress", 185, 5)
+deadlift = GymExercise("deadlift", 225, 6)
+press = GymExercise("press", 120, 8)
+
+print(squat.weight)
