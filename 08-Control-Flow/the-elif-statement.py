a8c59b44d53f8a0fbd07caf2f47d167898a13d48
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-04 04:37:36 +0100

    an exercise from Hacker Rank

--- 08-Control-Flow/the-elif-statement.py
+++ 08-Control-Flow/the-elif-statement.py
@@ -37,3 +37,14 @@ def negative_energy(a):
         
 print(negative_energy(-5))
 
+
+n = int(input("Enter "))
+
+if n % 2 != 0:
+    print("Weird")
+elif n in range(2, 6) and n % 2 == 0:
+    print("Not Weird")
+elif n in range(6, 21) and n % 2 == 0:
+    print("Weird")
+else:
+    print("Not Weird")
