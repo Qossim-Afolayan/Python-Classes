86b6f0ac586636888820ecfcdd2131d120a6b912
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-18 22:28:18 +0100

    exercise on introduction to unittest

--- 30-Testing-Code-The-Basics/the_assert_statement.py
+++ 30-Testing-Code-The-Basics/the_assert_statement.py
@@ -4,4 +4,31 @@ def add(x, y):
 
 print(add(3, 5))
 # print(add(3, "5")) #AssertionError
+import unittest
+class TestStringMethods(unittest.TestCase):
 
+    def test_split(self):
+
+      self.assertEqual("a-b-c".split("-"), ["a", "b", "c"])
+
+
+
+    def test_count(self):
+
+      self.assertEqual("beautiful".count("u"), 2)
+
+
+
+    def test_swapcase(self):
+        self.assertEqual("qossim".swapcase(), "QOSSIM")
+
+
+
+    def test_index(self):
+        self.assertEqual("qossim".index("q"), 0)
+
+
+
+if __name__ == "__main__":
+
+  unittest.main()
