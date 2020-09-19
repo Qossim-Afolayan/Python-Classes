e40eeb631d3a6e08fb0b4053702cf66d4860a6e1
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-19 11:52:37 +0100

    skipping test with @unittest.skip()

---
+++ 30-Testing-Code-The-Basics/skipping-tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestSkippedStuff(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtraction(self):
+        self.assertEqual(1 - 1, 0)
+    @unittest.skip("Yet to be implemented")
+    def test_multiply(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
