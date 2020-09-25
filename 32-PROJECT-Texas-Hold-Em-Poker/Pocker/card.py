c134961a9d684c6d830f76f960a90ea6ce9fb5e8
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-25 22:20:09 +0100

    Card class and few attributes

---
+++ 32-PROJECT-Texas-Hold-Em-Poker/Pocker/card.py
@@ -0,0 +1,10 @@
+class Card():
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return F"{self.rank} of {self.suit}"
+
+    def __repr__(self):
+        return F"Card('{self.rank}', '{self.suit}')"
