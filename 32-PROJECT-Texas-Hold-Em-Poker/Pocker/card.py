751c251ef1d3c405d994b488e15320775d437eb5
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-27 15:02:54 +0100

    The Card Class

--- 32-PROJECT-Texas-Hold-Em-Poker/Pocker/card.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/Pocker/card.py
@@ -1,5 +1,18 @@
 class Card():
+    SUITS = ("Hearts", "Clubs", "Spades", "Diamonds")
+
+    RANKS = (
+                "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King", "Ace"
+            )
     def __init__(self, rank, suit):
+        if rank not in self.RANKS:
+            raise ValueError(F"Invalid rank. Rank must be one of the following: {self.RANKS}")
+        
+        if suit not in self.SUITS:
+            raise ValueError(F"Invalid suit. Suit must be one of the following: {self.SUITS}")
+       
+
         self.rank = rank
         self.suit = suit
 
