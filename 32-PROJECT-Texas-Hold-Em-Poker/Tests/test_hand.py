2819bff4454d7c37431cd5c89283fe4a16321ca7
Qossim Afolayan <qossimafolayan@gmail.com>
2020-09-29 22:10:39 +0100

    The test to figure out three of a kind and a straight best rank

--- 32-PROJECT-Texas-Hold-Em-Poker/Tests/test_hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/Tests/test_hand.py
@@ -57,3 +57,33 @@ class HandTest(unittest.TestCase):
             hand.best_rank(),
             "Two Pair"
         )
+    def test_figures_out_three_of_a_kind_is_best_rank(self):
+        cards = [
+            Card(rank = "King", suit = "Clubs"),
+            Card(rank = "King", suit = "Hearts"),
+            Card(rank = "King", suit = "Diamonds"),
+            Card(rank = "Ace", suit = "Spades"),
+            Card(rank = "5", suit = "Clubs")
+        ]
+
+        hand = Hand(cards = cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Three of a Kind"
+        )
+    def test_figures_out_straight_is_best_rank(self):
+        cards = [
+            Card(rank = "6", suit = "Hearts"),
+            Card(rank = "7", suit = "Diamonds"),
+            Card(rank = "8", suit = "Spades"),
+            Card(rank = "9", suit = "Clubs"),
+            Card(rank = "10", suit = "Clubs")
+        ]
+
+        hand = Hand(cards = cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Straight"
+        )
