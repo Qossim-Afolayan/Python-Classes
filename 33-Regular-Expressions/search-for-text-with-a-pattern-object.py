7de49372872bb5ac29caf7ab3b47d14fbc785381
Qossim Afolayan <qossimafolayan@gmail.com>
2020-10-10 10:50:21 +0100

    re module

---
+++ 33-Regular-Expressions/search-for-text-with-a-pattern-object.py
@@ -0,0 +1,15 @@
+import re
+
+pattern = re.compile("flower")
+print(type(pattern))
+
+print(pattern.search("candy"))
+
+match = pattern.search("a red flower in field")
+print(type(match))
+
+if match:
+    print(match.group())
+    print(match.start())
+    print(match.end())
+    print(match.span())
